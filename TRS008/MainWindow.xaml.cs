3261b34e4efb73b6cef2ed613ebea8d4923cd1b6
fanqi <727996314@qq.com>
2018-10-19 21:00:18 +0800

    添加项目文件。

---
+++ TRS008/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System.IO;
+
+namespace TRS008
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TargetSearch(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if(result == true)
+            {
+                this.targetPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void ToDo(object sender, RoutedEventArgs e)
+        {
+            var targetPathValue = targetPath.Text;
+            var begin = dateBegin.Text;
+            var end = dateEnd.Text;
+
+            object oMissing = System.Reflection.Missing.Value;
+             
+        }
+    }
+}
